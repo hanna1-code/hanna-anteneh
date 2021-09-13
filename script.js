e5ccf07b488d296f7336911a377ddf976a2dffae
hanna1-code <85033654+hanna1-code@users.noreply.github.com>
2021-09-13 10:28:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-# hanna-anteneh
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    //animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = 'navLinkFade 0.5s ease forward ${index /7 + 1.5}s';
+        }
+    });
+   //burger animation
+    burger.classList.toggle('toggle');
+}
+navslide();
